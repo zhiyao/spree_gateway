44f588e0ec70747152b7ef8d1c879107f55d2683
Kenny Chan Zhi Yao <zhiyao.chan@gmail.com>
2017-10-18 16:33:05 +0800

    fix the attribute accessible missing preferences

--- app/models/spree/gateway/eway_rapid.rb
+++ app/models/spree/gateway/eway_rapid.rb
@@ -3,6 +3,8 @@ module Spree
     preference :login, :string
     preference :password, :string
 
+    attr_accessible :preferred_login, :preferred_password
+
     def provider_class
       ActiveMerchant::Billing::EwayRapidGateway
     end
